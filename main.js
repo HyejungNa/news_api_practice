1861f86e547fda9f86078684f9c0a104660a8a80
Hyejung Na <hyejung.na01@gmail.com>
2024-07-10 00:07:44 +1000

    added async,await

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-// const API_KEY = 'c5878c7156124f3580df6afb2aad27a8';
+const API_KEY = 'c5878c7156124f3580df6afb2aad27a8';
+let news = [];
 
-const getLatestNews = () => {
+const getLatestNews = async () => {
   // const url = new URL(
   //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   // );
-  const url = new URL(`https://news-api-prac.netlify.app/top-headlines`);
-  const response = fetch(url);
+  // news api사용
+  const url = new URL(`https://news-api-prac.netlify.app/top-headlines`); //코딩누나 api사용
+  const response = await fetch(url);
+  const data = await response.json();
+
+  news = data.articles;
+  console.log(news);
 };
 
 getLatestNews();
