5ee2a4150da59eda2c4cecc75af50e16ec31f0d7
Hyejung Na <hyejung.na01@gmail.com>
2024-07-09 22:19:38 +1000

    added noona api url

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const API_KEY = 'c5878c7156124f3580df6afb2aad27a8';
+// const API_KEY = 'c5878c7156124f3580df6afb2aad27a8';
+
 const getLatestNews = () => {
-  const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
-  );
+  // const url = new URL(
+  //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  // );
+  const url = new URL(`https://news-api-prac.netlify.app/top-headlines`);
   const response = fetch(url);
 };
 
