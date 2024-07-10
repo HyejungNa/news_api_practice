ef9fcb83c3173eb8cd86008f19bb28fd35eb36b6
Hyejung Na <hyejung.na01@gmail.com>
2024-07-11 01:36:04 +1000

    added error image

--- main.js
+++ main.js
@@ -42,11 +42,12 @@ const render = () => {
     .map((news) => {
       return `<div class="row news">
 <div class="col-lg-4">
-          <img class="news-img-size"
+                <img class="news-img-size"
                 src="${
                   news.urlToImage ||
                   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'
-                }" />
+                }"
+                onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';" />
           </div>
           <div class="col-lg-8">
             <h2>${news.title}</h2>
