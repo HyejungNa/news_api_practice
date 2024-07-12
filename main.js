6603bd6bcd32e533057e4f6707dc4ff2871343fd
Hyejung Na <hyejung.na01@gmail.com>
2024-07-12 13:31:34 +1000

    reractored code

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 const API_KEY = 'c5878c7156124f3580df6afb2aad27a8';
+
 let newsList = [];
 const menus = document.querySelectorAll('.menus button');
-const sideMenuList = document.querySelectorAll('.side-menu-list');
-
 menus.forEach((menu) => {
   menu.addEventListener('click', (event) => getNewsByCategory(event));
 });
 
-sideMenuList.forEach((sidemenu) => {
-  sidemenu.addEventListener('click', (event) => getNewsByCategory(event));
+const sideMenuList = document.querySelectorAll('.side-menu-list');
+sideMenuList.forEach((sideMenu) => {
+  sideMenu.addEventListener('click', (event) => getNewsByCategory(event));
 });
 
+let url = new URL(
+  // news api사용
+  // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  // 코딩누나 새 api 사용
+  `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
+);
+
 const openNav = () => {
-  document.getElementById('mySidenav').style.width = '250px';
+  document.getElementById('mySidenav').style.width = '115px';
 };
 
 const closeNav = () => {
@@ -39,79 +46,83 @@ const openSearchBox = () => {
   });
 };
 
+const getNews = async () => {
+  try {
+    const response = await fetch(url); // 비동기 호출
+    const data = await response.json(); // 비동기 호출
+
+    if (response.status === 200) {
+      if (data.articles.length === 0) {
+        throw new Error('No result for this search');
+      }
+      newsList = data.articles;
+      render();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    errorRender(error.message);
+  }
+};
+
 const getLatestNews = async () => {
   // news api사용
-  // const url = new URL(
+  // url = new URL(
   //   `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   // );
 
   // 코딩누나 새 api 사용
-  const url = new URL(
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines`
   );
 
-  const response = await fetch(url);
-  const data = await response.json();
-
-  newsList = data.articles;
-  render();
-  console.log(newsList);
+  getNews();
+  // console.log(newsList);
 };
 
 const getNewsByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
-  console.log(category);
 
   // news api사용
-  // const url = new URL(
+  // url = new URL(
   //   `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
   // );
 
   // 코딩누나 새 api 사용
-  const url = new URL(
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
   );
 
-  const response = await fetch(url); // 비동기 호출
-  const data = await response.json(); // 비동기 호출
-
-  newsList = data.articles;
-  render();
+  getNews();
 };
 
 const getNewsByKeyword = async () => {
   const keyword = document.getElementById('search-input').value;
-  console.log('keyword', keyword);
 
   // news api사용
-  // const url = new URL(
+  // url = new URL(
   //   `https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`
   // );
 
   // 코딩누나 새 api 사용
-  const url = new URL(
+  url = new URL(
     `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=us&country=us&q=${keyword}&apiKey=${API_KEY}`
   );
 
-  const response = await fetch(url); // 비동기 호출
-  const data = await response.json(); // 비동기 호출
-  console.log('data', data);
-
-  newsList = data.articles;
-  render();
+  getNews();
 };
 
 const render = () => {
   const newsHTML = newsList
     .map((news) => {
       return `<div class="row news">
-<div class="col-lg-4">
+                <div class="col-lg-4">
                 <img class="news-img-size"
                 src="${
                   news.urlToImage ||
                   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU'
                 }"
-                onerror="this.onerror=null;this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';" />
+                onerror="this.onerror=null; this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU';" />
           </div>
           <div class="col-lg-8">
             <h2>${news.title}</h2>
@@ -130,6 +141,13 @@ const render = () => {
     })
     .join('');
   document.getElementById('news-board').innerHTML = newsHTML;
+};
+
+const errorRender = (errorMessage) => {
+  const errorHTML = `<div class="alert alert-danger" role="alert">
+  ${errorMessage}
+</div>`;
+  document.getElementById('news-board').innerHTML = errorHTML;
 };
 
 getLatestNews();
